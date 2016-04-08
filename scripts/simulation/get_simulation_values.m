7eea3277727b64cc93e55d4eb2f4a35d061b0cfa
Kyle Lund <klund@hmc.edu>
2016-04-08 16:58:24 -0700

    filled in some things in matlab simulation value script

--- scripts/simulation/get_simulation_values.m
+++ scripts/simulation/get_simulation_values.m
@@ -5,13 +5,17 @@
 
 %% Plant matrices:
 
-A = [-.5 .5; 0 -2];
-B = [0; 2];
-C = [1 0];
+A = [-.5 .5; 0 -2]
+B = [0; 2]
+C = [1 0]
 
 %% Determine the equivalent discrete plant:
 
-
+D = 0;
+Ts = 0.1;
+dt_sys = c2d(ss(A,B,C,D),Ts);
+Ad = dt_sys.a
+Bd = dt_sys.b
 
 
 %% Determine the feedback gain vector K
@@ -21,13 +25,14 @@ C = [1 0];
 
 
 %% Determine the reference gain Kr:
-
-
+I = eye(2);
+Kr = -inv( (C-D*K)*inv(Ad-I-Bd*K)*Bd + D )
 
 
 %% Design the Observer: Determine the Observer gain vector L
 
-
+P_L = 0.2*P_K;
+L = acker(Ad',C',P_L).'
 
 
 
