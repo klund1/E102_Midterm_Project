7a28be6939d31e9bbfc6c3bc43a9b99ee0b6ca5d
Jesse Joseph <jjoseph@cs.hmc.edu>
2016-04-13 22:29:27 -0700

    commented arduino sketch

--- scripts/arduino/controller/controller.ino
+++ scripts/arduino/controller/controller.ino
@@ -56,20 +56,24 @@ void loop(){
 //This function implements the controller 
 //Observer, full-state feedback gains, and reference gains;
 float controller(float* xhat, float& yhat, float yn, float un, float rn) {
+  // yhat[n] = C*xhat[n];
   yhat = C[0]*xhat[0]+C[1]*xhat[1];
+  // xhat[n+1] = Ad*xhat[n] + Bd*u[n] + L*(y[n] - yhat[n])
   float new_xhat0 = Ad[0][0]*xhat[0] + Ad[0][1]*xhat[1] + Bd[0]*un + L[0]*(yn-yhat);
   float new_xhat1 = Ad[1][0]*xhat[0] + Ad[1][1]*xhat[1] + Bd[1]*un + L[1]*(yn-yhat);
   xhat[0] = new_xhat0;
   xhat[1] = new_xhat1;
   
+  // control law: u[n] = -K*xhat[n] + Kr*r[n]
   return -K[0]*xhat[0] - K[1]*xhat[1] + Kr*rn;
 }
 
-//This function ensures that the output is in a valid range for analogWrite
+//This function converts the 10-bit value from the ADC to a voltage value
 float voltage(int digital_value) {
  return 5.0*digital_value/1023;
 }
 
+//This function ensures that the output is in a valid range for analogWrite
 int digitalValue(float voltage) {
   if(voltage > 5.0) {
     return 255;
